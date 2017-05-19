c7bab64141240f6fd0daca57bcac0e5ba8388589
Priya Ranjan Singh <mail@priyaranjan.net>
2017-05-19 23:57:12 +0530

    add webpack production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,5 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: 'index.html',
-  inject: 'body',
-});
-const HotModuleReplacementPluginConfig = new webpack.HotModuleReplacementPlugin();
-const ExtractTextPluginConfig = new ExtractTextPlugin('style.css');
-
-module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-    }, {
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader'],
-      }),
-    }],
-  },
-  plugins: [
-    HtmlWebpackPluginConfig,
-    HotModuleReplacementPluginConfig,
-    ExtractTextPluginConfig,
-  ],
-  devServer: {
-    hot: true,
-    inline: true,
-    port: 9000,
-  },
+module.exports = (env) => {
+  const activeEnv = env || 'dev';
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  return require(`./webpack.${activeEnv}.js`);
 };
---
+++ webpack.prod.js
@@ -0,0 +1,36 @@
+const path = require('path');
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: 'index.html',
+  inject: 'body',
+});
+const ExtractTextPluginConfig = new ExtractTextPlugin('style-[hash].css');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: '[name]-[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    sourceMapFilename: '[name].map',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader'],
+      }),
+    }],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    ExtractTextPluginConfig,
+  ],
+};
