63c473d2f0009a425cc4cf5124d2601277973413
Priya Ranjan Singh <mail@priyaranjan.net>
2017-06-09 23:43:09 +0530

    add react router for routing

---
+++ src/components/userDashboard/userDashboard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import api from '../../utilities/api/api';
+import '../../index.scss';
+
+class UserDashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      track: {},
+    };
+  }
+
+  componentDidMount() {
+    const username = 'crashuniverse';
+    api(username)
+      .then((track) => {
+        this.setState({
+          track,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>weekly love</h1>
+          <div>The favorite track you were listening to last week.</div>
+        </header>
+        <div className="track-container">
+          <div className="image">
+            <img src={this.state && this.state.track && this.state.track.image} alt="" />
+          </div>
+          <div>
+            <div className="track">
+              {this.state && this.state.track && this.state.track.name}
+            </div>
+            <div className="artist">
+              {this.state && this.state.track && this.state.track.artist}
+            </div>
+            <div className="count">
+              {
+                this.state && this.state.track && this.state.track.playcount &&
+                `${this.state.track.playcount} times in week of
+                ${this.state.track.weekStartDate} - ${this.state.track.weekEndDate}`
+              }
+            </div>
+          </div>
+        </div>
+        <footer>API from <a href="https://www.last.fm/api" target="_blank" rel="noopener noreferrer">lastfm</a></footer>
+      </div>
+    );
+  }
+}
+
+export default UserDashboard;
--- src/index.js
+++ src/index.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import api from './utilities/api/api';
-import './index.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UserDashboard from './components/userDashboard/userDashboard';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      track: {},
-    };
-  }
-
-  componentDidMount() {
-    const username = 'crashuniverse';
-    api(username)
-      .then((track) => {
-        this.setState({
-          track,
-        });
-      });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>weekly love</h1>
-          <div>The favorite track you were listening to last week.</div>
-        </header>
-        <div className="track-container">
-          <div className="image">
-            <img src={this.state && this.state.track && this.state.track.image} alt="" />
-          </div>
-          <div>
-            <div className="track">
-              {this.state && this.state.track && this.state.track.name}
-            </div>
-            <div className="artist">
-              {this.state && this.state.track && this.state.track.artist}
-            </div>
-            <div className="count">
-              {
-                this.state && this.state.track && this.state.track.playcount &&
-                `${this.state.track.playcount} times in week of
-                ${this.state.track.weekStartDate} - ${this.state.track.weekEndDate}`
-              }
-            </div>
-          </div>
-        </div>
-        <footer>API from <a href="https://www.last.fm/api" target="_blank" rel="noopener noreferrer">lastfm</a></footer>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Route path="/" component={UserDashboard} />
+  </Router>
+);
 
 render(<App />, document.getElementById('root'));
